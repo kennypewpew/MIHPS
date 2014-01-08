1266772f826ab2ffd52ebba23cb108c2cea89fe8
Kenny P <ken.peou@gmail.com>
2014-01-08 10:10:48 +0100

    added save_mesh to save the obstacle map

--- PROJET/mesh_functions.h
+++ PROJET/mesh_functions.h
@@ -10,5 +10,6 @@ void generate_circle(double *centre, int rayon, double *mesh);
 void generate_rectangle(double *down_left, double *up_right, double *mesh);
 void save_dist(double* dist);
 void save_path(double* dist, pt arrivee, pt* path);
+void save_mesh(double* mesh);
 
 #endif
